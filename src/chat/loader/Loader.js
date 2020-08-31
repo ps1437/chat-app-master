7843d23d9266c6dc81febf58b7e3c967a59803e1
ps1437 <praveen369soni@gmail.com>
2020-08-31 21:58:43 +0530

    Loader added

---
+++ src/chat/loader/Loader.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Loader =()=>{
+    return(
+        <div class="loader">
+        <div id="ld3">
+          <div>
+          </div>
+          <div>
+          </div>
+          <div>
+          </div>
+          <div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Loader;
--- src/chat/router/Routers.js
+++ src/chat/router/Routers.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route ,Redirect} from "react-router-dom";
 import Auth from '../Auth';
+import Loader from '../loader/Loader';
 
 const Home = React.lazy(() => import("../Home"));
 const Chat = React.lazy(() => import("../Chat"));
@@ -8,11 +9,11 @@ const Chat = React.lazy(() => import("../Chat"));
 const Routers = () => {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader/>}>
         <Route exact path="/" component={Home} />
       </Suspense>
 
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader/>}>
         <PrivateRoute path="/chat" component={Chat} />
       </Suspense>
     </Router>
