daaebdbf3e0f3b921dcc97e91ad2bfddc5066b49
ps1437 <praveen369soni@gmail.com>
2020-09-02 14:19:28 +0530

    UI modified

--- src/chat/router/Routers.js
+++ src/chat/router/Routers.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Route ,Redirect} from "react-router-dom";
-import Auth from '../Auth';
-import Loader from '../loader/Loader';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Auth from "../Auth";
+import Loader from "../loader/Loader";
 
 const Home = React.lazy(() => import("../Home"));
 const Chat = React.lazy(() => import("../Chat"));
@@ -9,11 +9,11 @@ const Chat = React.lazy(() => import("../Chat"));
 const Routers = () => {
   return (
     <Router>
-      <Suspense fallback={<Loader/>}>
+      <Suspense fallback={<Loader />}>
         <Route exact path="/" component={Home} />
       </Suspense>
 
-      <Suspense fallback={<Loader/>}>
+      <Suspense fallback={<Loader />}>
         <PrivateRoute path="/chat" component={Chat} />
       </Suspense>
     </Router>
